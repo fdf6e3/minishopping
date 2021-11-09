acdd7b5c9e9b798ae40982fc73945b703bb7deed
YOUNGHYUN BAE <yhbae97@gmail.com>
2021-11-10 03:22:36 +0900

    ADD
    Fetch the items from the JSON file

--- src/main.js
+++ src/main.js
@@ -1,7 +1,21 @@
-// data.json으로부터 동적으로 items를 불러오자
+// Fetch the items from the JSON file
+function loadItems() {
+  return fetch('data/data.json')
+  //fetch()로 JSON 데이터 간단히 불러오기
+    .then(response => response.json())
+    //개발자 툴로 console, network 잘 들어왔나 확인
+    .then(json => json.items);
+    //loadItems라는 함수는
+    //fetch라는 API를 통하여 데이터를 받고
+    //성공적으로 받아왔다면 json으로 변환하고
+    //json 안에 있는 items 값을 받아옴
+}
+
+//main
 loadItems()
   .then(items => {
-    displayItems(items)
-    setEventListeners(items)
+    console.log(items);
+    //displayItems(items)
+    //setEventListeners(items)
   })
-  .catch(console.log)
+  .catch(console.log);
