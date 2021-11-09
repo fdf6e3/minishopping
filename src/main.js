5bf3220c2c7acf37bd6cfab97145d0556a4ffc07
YOUNGHYUN BAE <yhbae97@gmail.com>
2021-11-10 04:35:51 +0900

    ADD
    1. Update the list with the given items
    2. Create HTML list item from the given data item

--- src/main.js
+++ src/main.js
@@ -11,11 +11,30 @@ function loadItems() {
     //json 안에 있는 items 값을 받아옴
 }
 
+//Update the list with the given items 
+function displayItems(items) {
+  const container = document.querySelector('.items');
+  //items 클래스로 정의된 ul 태그를 container로 정의
+  container.innerHTML = items.map(item => createHTMLString(item)).join('')
+  //JSON에서 받아온 각각의 item 객체들을 HTML li 태그의 배열 그룹으로 변환해서 container에 각각 추가
+}
+
+//Create HTML list item from the given data item
+function createHTMLString(item) {
+  return `
+    <li class="item">
+      <img src="${item.image}}" alt="${item.type}" class="item_thumbnail">
+      <span class="item_description">${item.gender}, ${item.size}</span>
+    </li>
+  `;
+  //원하는 문자열 형태로 리턴해주는 함수
+}
+
 //main
 loadItems()
   .then(items => {
     console.log(items);
-    //displayItems(items)
+    displayItems(items)
     //setEventListeners(items)
   })
   .catch(console.log);
