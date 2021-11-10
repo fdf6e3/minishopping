12a5cf287706a4963e78128d596f448a4e3f44ab
YOUNGHYUN BAE <yhbae97@gmail.com>
2021-11-11 02:32:06 +0900

    ADD
    Handle button click

--- src/main.js
+++ src/main.js
@@ -15,7 +15,7 @@ function loadItems() {
 function displayItems(items) {
   const container = document.querySelector('.items');
   //items 클래스로 정의된 ul 태그를 container로 정의
-  container.innerHTML = items.map(item => createHTMLString(item)).join('')
+  container.innerHTML = items.map(item => createHTMLString(item)).join('');
   //JSON에서 받아온 각각의 item 객체들을 HTML li 태그의 배열 그룹으로 변환해서 container에 각각 추가
 }
 
@@ -23,18 +23,47 @@ function displayItems(items) {
 function createHTMLString(item) {
   return `
     <li class="item">
-      <img src="${item.image}}" alt="${item.type}" class="item_thumbnail">
+      <img src="${item.image}" alt="${item.type}" class="item_thumbnail">
       <span class="item_description">${item.gender}, ${item.size}</span>
     </li>
   `;
   //원하는 문자열 형태로 리턴해주는 함수
 }
 
+//Handle button click
+function onButtonClick(event, items) {
+  //HTML buttons에 가서 data-key, data-value custom attributes 생성
+  //console.log(event.target.dataset.key); 잘 나오는 지
+  //console.log(event.target.dataset.value); 확인
+  const dataset = event.target.dataset;
+  const key = dataset.key;
+  const value = dataset.value;
+
+  if (key == null || value == null) {
+    return;
+  }
+  //아무 값도 없을 때는 아무것도 리턴 x
+
+  displayItems(items.filter(item => item[key] === value));
+  //key와 value가 잁치하는 배열끼리 보여주기
+}
+
+function setEventListeners(items) {
+  const logo = document.querySelector('.logo');
+  const buttons = document.querySelector('.buttons');
+  //효율을 위해 아이템 각각이 아니라 logo, buttons 컨테이너에 이벤트 위임
+  logo.addEventListener('click', () => displayItems(items));
+  //logo를 click하면 전체 아이템이 보여지고
+  buttons.addEventListener('click', event => onButtonClick(event, items));
+  //button을 click하면 동작을 알맞게 정의한 함수 실행
+  //on~으로 함수 naming하는 게 어떤 기능 나타내는 함수인지 직관적
+}
+
 //main
 loadItems()
   .then(items => {
-    console.log(items);
-    displayItems(items)
-    //setEventListeners(items)
+    //console.log(items);
+    displayItems(items);
+    setEventListeners(items);
   })
   .catch(console.log);
